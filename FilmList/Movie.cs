731db67a6dd9712a11be9218afb732ae2b554055
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-22 13:27:18 +0300

    Movie List Completed

---
+++ FilmList/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieList
+{
+    internal class Movie
+    {
+        private string? _name;
+        private double _rating;
+
+        public Movie(string? name, double rating)
+        {
+            Name = name;
+            Rating = rating;
+        }
+
+        public string? Name { get => _name; set => _name = value; }
+        public double Rating { get => _rating; set => _rating = value; }
+    }
+}
---
+++ FilmList/Program.cs
@@ -0,0 +1,56 @@
+﻿using MovieList;
+
+bool isFinished = false;
+
+string name, userDecide;
+
+double rating;
+
+List<Movie> movies = new List<Movie>();
+
+//kullanici kontrolunde veri girisi
+do
+{
+    //kullanicidan film ismini alir
+    Console.Write("Lutfen filmin ismini giriniz: ");
+    name = Console.ReadLine()!;
+
+    //kullanicidan film puanini alir
+    Console.Write("Lutfen filmin puanini giriniz: ");
+    rating = double.Parse(Console.ReadLine()!);
+
+    //film listesine ekleme yapar
+    movies.Add(new Movie(name: name, rating: rating));
+
+    //kullaniciya ekleme islemine devam etmek istedigini sorar
+    Console.Write("Yeni bir film eklemek ister misiniz? (e/h): ");
+    userDecide = Console.ReadLine()!;
+    Console.WriteLine();
+
+    //cevaba gore donguyu sonlandirir
+    isFinished = (userDecide == "e") ? false : true;
+}
+while (!isFinished);
+
+//tum filmleri listeler
+Console.WriteLine("Tum Filmler");
+movies.ForEach(movie => Console.WriteLine($"Film ismi: {movie.Name} Puanı: {movie.Rating}"));
+Console.WriteLine();
+
+//puani 4 ile 9 arasinda olan filmleri listeler
+Console.WriteLine("Puani 4 ile 9 Arasinda Olan Filmler: ");
+var tempMovieList = movies.Where(movie => (movie.Rating >= 4.0) && (movie.Rating <= 9.0));
+foreach (var movie in tempMovieList)
+{
+    Console.WriteLine($"Film ismi: {movie.Name} Puanı: {movie.Rating}");
+}
+Console.WriteLine();
+
+//A harfi ile baslayan filmleri listeler
+Console.WriteLine("Ismi 'A' ile Baslayan Filmler");
+tempMovieList = movies.Where(movie => movie.Name.StartsWith('A'));
+foreach (var movie in tempMovieList)
+{
+    Console.WriteLine($"Film ismi: {movie.Name} Puanı: {movie.Rating}");
+}
+Console.WriteLine();
