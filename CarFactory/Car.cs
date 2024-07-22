cdd2c92626b36a8d672849db5d8fb71f4f108183
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-22 16:09:07 +0300

    Car Factory Completed
    Week 5 final project.

---
+++ CarFactory/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactory
+{
+    internal class Car
+    {
+        private DateTime _manufactorDate;
+        private string? _serialNumber;
+        private string? _brand;
+        private string? _model;
+        private string? _color;
+        private int _doorCount;
+
+        public Car(string? serialNumber, string? brand, string? model, string? color, int doorCount)
+        {
+            ManufactorDate = DateTime.Now;
+            SerialNumber = serialNumber;
+            Brand = brand;
+            Model = model;
+            Color = color;
+            DoorCount = doorCount;
+        }
+
+        public DateTime ManufactorDate { get => _manufactorDate; set => _manufactorDate = value; }
+        public string? SerialNumber { get => _serialNumber; set => _serialNumber = value; }
+        public string? Brand { get => _brand; set => _brand = value; }
+        public string? Model { get => _model; set => _model = value; }
+        public string? Color { get => _color; set => _color = value; }
+        public int DoorCount { get => _doorCount; set => _doorCount = value; }
+    }
+}
---
+++ CarFactory/Program.cs
@@ -0,0 +1,91 @@
+﻿using CarFactory;
+
+bool isFinished = false;
+
+string userDecide;
+
+List<Car> cars = new List<Car>();
+
+do
+{
+    //kullanici karari alinir
+    Console.Write("Araba uretmek ister misiniz? (e/h): ");
+    userDecide = Console.ReadLine()!.ToLower();
+
+    switch (userDecide)
+    {
+        case "e":
+            //arac uretilir ve listeye eklenir
+            Car car = CreateCar();
+            cars.Add(car);
+            break;
+        case "h":
+            //araclar listelenir ve dongu sonlandirilir
+            GetAllCars(cars);
+            isFinished = true;
+            break;
+        default:
+            //hata kontrolu
+            Console.WriteLine("Hatali giris yaptiniz. Tekrar deneyiniz.");
+            break;
+    }
+} while (!isFinished);
+
+Car CreateCar()
+{
+    string serialNumber, brand, model, color;
+    int doorCount;
+
+    //arac bilgileri alinir
+    Console.Write("Seri numarasini giriniz: ");
+    serialNumber = Console.ReadLine()!;
+    Console.WriteLine();
+
+    Console.Write("Markayi giriniz: ");
+    brand = Console.ReadLine()!;
+    Console.WriteLine();
+
+    Console.Write("Modeli giriniz: ");
+    model = Console.ReadLine()!;
+    Console.WriteLine();
+
+    Console.Write("Rengi giriniz: ");
+    color = Console.ReadLine()!;
+    Console.WriteLine();
+
+doorCount:
+    Console.Write("Kapi sayisini giriniz: ");
+
+    //hata kontrolu
+    try
+    {
+        doorCount = int.Parse(Console.ReadLine()!);
+    }
+    catch (Exception)
+    {
+        // hata alinirsa mesaj yazdirilir ve onceki kisma geri atlanir
+        Console.WriteLine("Lutfen gecerli bir sayi giriniz.");
+        goto doorCount;
+    }
+    Console.WriteLine();
+
+    Car car = new Car(serialNumber, brand, model, color, doorCount);
+
+    return car;
+}
+
+void GetAllCars(List<Car> cars)
+{
+    //listeki her aracın bilgileri konsola yazdirilir
+    foreach (var car in cars)
+    {
+        Console.WriteLine("---------------------------------");
+        //Console.WriteLine($"Uretim tarihi: {car.ManufactorDate}");
+        Console.WriteLine($"Seri numarasi: {car.SerialNumber}");
+        Console.WriteLine($"Markasi: {car.Brand}");
+        //Console.WriteLine($"Modeli: {car.Model}");
+        //Console.WriteLine($"Rengi: {car.Color}");
+        //Console.WriteLine($"Kapi sayisi: {car.DoorCount}");
+        Console.WriteLine("---------------------------------");
+    }
+}
