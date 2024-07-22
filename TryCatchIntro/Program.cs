6a7a3c6764a6da32f1fc1115e714a5eceaaed86a
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-22 14:07:00 +0300

    TryCatch Intro Completed

---
+++ TryCatchIntro/Program.cs
@@ -0,0 +1,25 @@
+﻿int number;
+bool isFinished = false;
+
+do
+{
+    Console.Write("Lutfen bir sayi giriniz: ");
+
+    //hata kontrolu
+    try
+    {
+        //kullanicidan giris alma
+        number = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine($"Girilen sayinin karesi: {Math.Pow(number, 2)}");
+
+        isFinished = true;
+    }
+    catch (Exception exception)
+    {
+        // hata mesaji
+        Console.WriteLine("Gecersiz giris!");
+    }
+
+    
+} while (!isFinished);
