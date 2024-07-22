5f0ae8d4364e2011ff2de9e23083deb7382496b5
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-22 13:03:53 +0300

    Coffee List Completed

---
+++ CoffeeList/Program.cs
@@ -0,0 +1,15 @@
+﻿List<string> coffeeNames = new List<string>();
+
+//kullanicidan kahve isimlerini alir
+Console.Write("Lutfen sevdiginiz kahveleri aralarina virgul koyarak yaziniz: ");
+var names = Console.ReadLine()!;
+Console.WriteLine();
+
+//girilen degeri parcalayarak listeye ekler
+var nameList = names.Split(',');
+coffeeNames.AddRange(nameList);
+
+Console.WriteLine("Girilen Kahve Isimleri");
+
+//listedeki degerleri yazdirir
+coffeeNames.ForEach(coffeeName => Console.WriteLine($"Kahve {coffeeNames.IndexOf(coffeeName) + 1}: {coffeeName}"));
