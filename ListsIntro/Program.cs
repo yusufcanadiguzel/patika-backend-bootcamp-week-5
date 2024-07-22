cf23a9ccb31e5394834a6729da866c45e059e92c
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-22 12:48:38 +0300

    List Intro Completed

---
+++ ListsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿//Bu Pratik'te sizden ziyaretçilerin isimlerini eklediğimiz bir liste oluşturmanızı ve bu liste içerisinde for ya da foreach döngüsü ile dönerek isimlerini tek tek ekrana yazdırmanız isteniyor.
+
+List<string> guests = new List<string>()
+{
+    "Bülent Ersoy",
+    "Ajda Pekkan",
+    "Ebru Gündeş",
+    "Hadise",
+    "Hande Yener",
+    "Tarkan",
+    "Funda Arar",
+    "Demet Akalın",
+};
+
+foreach (var guest in guests)
+    Console.WriteLine($"{guests.IndexOf(guest) + 1} - {guest}");
